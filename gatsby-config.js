f13ff9fbb163f354646fca4c0c52e2d3b2a7ca99
Travis Amaral <travis@konghq.com>
2018-05-07 20:04:26 -0700

    feat(*): Add type & GA settings + index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,57 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-41294706-1",
+        head: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Gothic\+A1\:400,600,700, 900` // you can also specify font weights and styles
+        ]
+      }
+    }
+  ],
+  siteMetadata: {
+    pathPrefix: "",
+    siteUrl: "http://travisamaral.com",
+    title: "Travis Amaral - Front-End Developer located in San Jose, CA",
+    author: "Travis Amaral",
+    description:
+      "Travis Amaral is a front-end developer with a focus on building fast, readable, and responsive UI's based out of San Jose, CA.",
+    social: {
+      codepen: "https://codepen.io/travispamaral",
+      linkedin: "https://linkedin.com/in/travispamaral",
+      github: "https://www.github.com/travispamaral"
+    },
+    projects: [
+      {
+        title: 'proj 1',
+        url: 'project.com',
+        description: 'Some description'
+      },
+      {
+        title: 'proj 2',
+        url: 'project2.com',
+        description: 'Some description'
+      },
+      {
+        title: 'proj 3',
+        url: 'project3.com',
+        description: 'Some description'
+      },
+      {
+        title: 'proj 4',
+        url: 'project4.com',
+        description: 'Some description'
+      },
+    ]
+  },
 }
---
+++ src/html.js
@@ -0,0 +1,54 @@
+import React from "react"
+
+let globalStyles = require(`!raw-loader!../assets/scss/globals.scss`)
+let stylesSt
+if (process.env.NODE_ENV === `production`) {
+  try {
+    stylesStr = require(`!raw-loader!../public/styles.css`)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+module.exports = class HTML extends React.Component {
+  render() {
+    let css
+    let globalCss = (
+      <style
+        dangerouslySetInnerHTML={{ __html: globalStyles }}
+      />
+    )
+    if (process.env.NODE_ENV === `production`) {
+      css = (
+        <style
+          id="gatsby-inlined-css"
+          dangerouslySetInnerHTML={{ __html: stylesStr }}
+        />
+      )
+    }
+    return (
+      <html {...this.props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          {this.props.headComponents}
+          {globalCss}
+          {css}
+        </head>
+        <body {...this.props.bodyAttributes}>  
+          {this.props.preBodyComponents}
+          <div
+            key={`body`}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+        </body>
+      </html>
+    )
+  }
+}
