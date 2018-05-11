41c6956c624cae36a46e933664cb4ec3e76240bd
Travis Amaral <travis@konghq.com>
2018-05-11 08:33:03 -0700

    refactor(contact) Move inline styles to scss file & style mobile

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,37 +1,23 @@
 import React, { Component } from 'react'
 
+import '../../assets/scss/pages/contact.scss'
+
 import Header from '../components/Header/Header'
-import Footer from '../components/Footer/Footer'
 import RightSlice from '../components/Slice/RightSlice'
 import Form from '../components/ContactForm/Form'
 
-const styles = {
-  width: '700px',
-  minHeight: '420px',
-  borderRadius: '4px',
-  backgroundColor: '#fff',
-  boxShadow: '0 2px 4px rgba(50, 50, 93, .1), 0 15px 35px rgba(50, 50, 93, .15), 0 5px 15px rgba(0, 0, 0, .1)',
-  padding: '1rem 2rem',
-  marginTop: '4rem',
-}
-
-class Contact extends Component {
-  render() {
-    return (
-      <div className="contact-page">
-        <Header />  
-        <div className="content">
-          <h1>Contact</h1>
-          <p>Want to work together? Ask me a question or just say hi? Shoot me a message!</p>
-          <div style={styles}>
-            <Form />
-          </div>
+export default ({ data }) => {
+  return (
+    <div className="contact-page">
+      <Header />  
+      <div className="content">
+        <h1>Contact</h1>
+        <p>Want to work together? Ask me a question or just say hi? Shoot me a message!</p>
+        <div className="form-wrapper">
+          <Form />
         </div>
-        <Footer />
-        <RightSlice />
       </div>
-    );
-  }
+      <RightSlice />
+    </div>
+  )
 }
-
-export default Contact;
