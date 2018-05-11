e247b2540de1e37a1ed8c3ae9f8281bf29fa6a8c
Travis Amaral <travis@konghq.com>
2018-05-11 08:40:57 -0700

    fix(form) testing form submission for netlify

--- src/components/ContactForm/Form.js
+++ src/components/ContactForm/Form.js
@@ -29,26 +29,26 @@ class Form extends Component {
 
     // delete (state.validationMessage)
 
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: this.encode({ "form-name": "contact", ...this.state })
-    })
-      .then(() => {
-        document.querySelectorAll('input, textarea').forEach(item => {
-          item.value = '';
-        })
-        this.setState({
-          validationMessage: 'Thanks for your message!',
-          validationClass: ''
-        })
-      })
-      .catch(error => {
-        this.setState({
-          validationMessage: 'Uh oh! It looks like something went wrong. You can try again or just send me an email at <strong>travispamaral@gmail.com</strong>.',
-          validationClass: 'has-error'
-        })
-      })
+    // fetch("/", {
+    //   method: "POST",
+    //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    //   body: this.encode({ "form-name": "contact", ...this.state })
+    // })
+      // .then(() => {
+      //   document.querySelectorAll('input, textarea').forEach(item => {
+      //     item.value = '';
+      //   })
+      //   this.setState({
+      //     validationMessage: 'Thanks for your message!',
+      //     validationClass: ''
+      //   })
+      // })
+      // .catch(error => {
+      //   this.setState({
+      //     validationMessage: 'Uh oh! It looks like something went wrong. You can try again or just send me an email at <strong>travispamaral@gmail.com</strong>.',
+      //     validationClass: 'has-error'
+      //   })
+      // })
   }
 
   render() {
@@ -56,22 +56,18 @@ class Form extends Component {
       <div className="contact-card">
         <form
           name="contact-form"
-          method="POST"
+          method="post"
           data-netlify="true"
           data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}>
-          <p hidden>
-            <label>
-              Don’t fill this out: <input name="bot-field" onChange={this.handleChange} />
-            </label>
-          </p>
+          >
+          <input type="hidden" name="bot-field" />
           <div className="row">
             <label>Full Name</label>
             <input
               type="text"
               name="name"
               placeholder="John Doe"
-              onChange={this.handleChange}
+              // onChange={this.handleChange}
               required />
 		      </div>
           <div className="row">
@@ -80,7 +76,7 @@ class Form extends Component {
               type="email"
               name="email"
               placeholder="john.doe@gmail.com"
-              onChange={this.handleChange}
+              // onChange={this.handleChange}
               required />
           </div>
           <div className="row">
@@ -89,7 +85,7 @@ class Form extends Component {
               type="text"
               name="message"
               placeholder="What would you like to say?"
-              onChange={this.handleChange}
+              // onChange={this.handleChange}
               required></textarea>
           </div>
           <div className="submit-row">
