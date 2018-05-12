a38108cf40fbe50cb7e5ce69bf94920ebd16133b
Travis Amaral <travis@konghq.com>
2018-05-12 16:47:46 -0700

    feat(contact): remove form and add mailto

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,6 +6,21 @@ import Header from '../components/Header/Header'
 import RightSlice from '../components/Slice/RightSlice'
 import Form from '../components/ContactForm/Form'
 
+const ButtonStyles = {
+  height: '40px',
+  lineHeight: '40px',
+  padding: '0 14px',
+  outline: 'none',
+  fontSize: 'inherit',
+  border: 'none',
+  borderRadius: '4px',
+  boxShadow: '0 4px 6px rgba(50, 50, 93, .11), 0 1px 3px rgba(0, 0, 0, .08)',
+  backgroundColor: '#5863EB',
+  color: '#fff',
+  cursor: 'pointer',
+  transition: 'all .15s ease',
+}
+
 export default ({ data }) => {
   return (
     <div className="contact-page">
@@ -13,9 +28,7 @@ export default ({ data }) => {
       <div className="content">
         <h1>Contact</h1>
         <p>Want to work together? Ask me a question or just say hi? Shoot me a message!</p>
-        <div className="form-wrapper">
-          <Form />
-        </div>
+        <a style={ButtonStyles} href="mailto:travispamaral@gmail.com">Email Me</a>
       </div>
       <RightSlice />
     </div>
