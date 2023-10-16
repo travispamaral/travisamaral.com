19a7839ad571b0b1bf6f4268de8b42ce443a8c7f
Travis Amaral <travis.amaral@shopify.com>
2023-10-16 07:05:48 -0700

    fix quote

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -49,7 +49,7 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              Hello 👋🏼, I&quot;m Travis Amaral. I live in sunny California and
+              Hello 👋🏼, I&apos;m Travis Amaral. I live in sunny California and
               work for Shopify.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
